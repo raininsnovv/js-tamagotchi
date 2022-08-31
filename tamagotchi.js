eb011b21ca2290d5205e437b830bdb9e5a34e133
raininsnovv <raininsnovv@gmail.com>
2022-08-31 10:06:13 +0300

    Сделал небольшую поправку

--- tamagotchi.js
+++ tamagotchi.js
@@ -1,33 +1,33 @@
 const tamogotchi = {
-    name: 'Mich',
-    meal: 1,
-    energy: 2,
-    mood: 4,
-    haveCar:false,
-    modelCar:'none',
-    money:0,
-    health:'alive',
-    cheating(){
-        if(this.money=0){
-            this.money += 1000000
-            this.mood += 10
-        }
-        else {
-            this.mood = 0
-        }
-    },
-    giveCar(){
-    if (this.energy=0){
-        this.haveCar = true
-        this.modelCar = 'Ford'
+  name: "Mich",
+  meal: 1,
+  energy: 2,
+  mood: 4,
+  haveCar: false,
+  modelCar: "none",
+  money: 0,
+  health: "alive",
+  cheating() {
+    if ((this.money = 0)) {
+      this.money += 1000000;
+      this.mood += 10;
+    } else {
+      this.mood = 0;
     }
-
-},
-    checkingHealth(){
-        if(this.energy<0 && this.mood<0){
-            this.health = 'dead'
-        return 'i am so sorry,bro'
-        }
-    },
-}
-  console.log(tamogotchi)
+  },
+  giveCar() {
+    if ((this.energy = 0)) {
+      this.haveCar = true;
+      this.modelCar = "Ford";
+    }
+  },
+  getStatus() {
+    if (this.energy <= 0 && this.mood <= 0) {
+      this.health = "dead";
+      return "i am so sorry,bro";
+    } else {
+      return "shut up,guy";
+    }
+  },
+};
+console.log(tamogotchi);
