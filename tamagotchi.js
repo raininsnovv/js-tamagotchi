35c2e4ba1953b622895573cacc57b43224a39fab
raininsnovv <raininsnovv@gmail.com>
2022-08-31 11:48:04 +0300

    Переделал все по совету наставника

--- tamagotchi.js
+++ tamagotchi.js
@@ -3,31 +3,64 @@ const tamogotchi = {
   meal: 1,
   energy: 2,
   mood: 4,
-  haveCar: false,
-  modelCar: "none",
-  money: 0,
-  health: "alive",
-  cheating() {
-    if ((this.money = 0)) {
-      this.money += 1000000;
-      this.mood += 10;
-    } else {
-      this.mood = 0;
-    }
-  },
-  giveCar() {
-    if ((this.energy = 0)) {
-      this.haveCar = true;
-      this.modelCar = "Ford";
-    }
-  },
+  hasFriend: false,
+  howMuchMoney: 1000,
+  married: "женат",
+  nameWife: "Sara",
   getStatus() {
-    if (this.energy <= 0 && this.mood <= 0) {
-      this.health = "dead";
-      return "i am so sorry,bro";
-    } else {
-      return "shut up,guy";
+    let meal = "Меца вац со";
+    let mood = "Я счастлив";
+    let energy = "Энергии полно";
+    if (this.meal < 3) {
+      meal = `Я голоден(${meal})`;
     }
+    if (this.mood < 3) {
+      mood = `Мне скучно(${mood})`;
+    }
+    if (this.energy < 3) {
+      energy = `Я хочу спать(${energy})`;
+    }
+    if (this.meal <= 0 || this.energy <= 0 || this.mood <= 0) {
+      console.log(
+        `${this.name} не заслуживал такого конца, но ты довёл его...`
+      );
+    } else {
+      console.log(
+        `Имя: ${this.name}, Еда: ${this.meal}, Энергия:${this.energy}, Настроение: ${this.mood}, Есть друг: ${this.hasFriend}, Сколько денег?: ${this.howMuchMoney}, Женат?: ${this.married}, Имя жены: ${this.nameWife}.`
+      );
+    }
+  },
+  setName(newName) {
+    this.name = newName;
+    console.log(`Теперь у питомца новое имя: ${newName}`);
+  },
+  eat() {
+    if (this.meal < 5) this.meal++;
+    this.mood--;
+    console.log(`${this.name} вкусно покушал.`);
+  },
+  sleep() {
+    if (this.energy < 5) this.energy++;
+    this.meal--;
+    console.log(`${this.name} хорошенько поспал.`);
+  },
+  play() {
+    if (this.mood < 5) this.mood++;
+    this.energy--;
+    console.log(`${this.name} поиграл с женой.`);
+  },
+  divorcedHisWife() {
+    this.married = "не женат";
+    console.log("Жена была непослушной!");
   },
 };
-console.log(tamogotchi);
+tamogotchi.setName("Jose");
+tamogotchi.getStatus();
+tamogotchi.eat();
+tamogotchi.getStatus();
+tamogotchi.sleep();
+tamogotchi.getStatus();
+tamogotchi.play();
+tamogotchi.getStatus();
+tamogotchi.divorcedHisWife();
+tamogotchi.getStatus();
